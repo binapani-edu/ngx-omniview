5429864c183d5ea8e4b6e1c177602ff3a6cc8443
Atanu Sarkar <mratanusarkar@gmail.com>
2025-10-29 13:31:04 +0530

    update: rename component selector to 'omniview'
    ```
    <omniview
      [data]="myContent"        // ← "Here's the content"
      [format]="'markdown'">    // ← "Render it as markdown"
    </omniview>
    ```

--- projects/ngx-omniview/src/lib/ngx-omniview.component.ts
+++ projects/ngx-omniview/src/lib/ngx-omniview.component.ts
@@ -1,15 +1,93 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
+/**
+ * Supported content formats for rendering
+ */
+export type OmniviewFormat = 
+  | 'text' 
+  | 'html' 
+  | 'markdown' 
+  | 'latex' 
+  | 'mathjax' 
+  | 'json' 
+  | 'code';
+
+/**
+ * OmniviewComponent - Universal content renderer
+ * 
+ * Renders raw string data in various formats including text, HTML, Markdown,
+ * LaTeX, MathJax, JSON, and syntax-highlighted code.
+ * 
+ * @example
+ * ```html
+ * <omniview [data]="content" [format]="'markdown'"></omniview>
+ * ```
+ */
 @Component({
-  selector: 'omniview-ngx-omniview',
+  selector: 'omniview',
   imports: [],
   template: `
-    <p>
-      ngx-omniview works!
-    </p>
+    <div class="omniview-content">
+      {{ renderedContent }}
+    </div>
   `,
-  styles: ``
+  styles: `
+    :host {
+      display: block;
+      width: 100%;
+      height: 100%;
+    }
+
+    .omniview-content {
+      width: 100%;
+      height: 100%;
+      white-space: pre-wrap;
+      word-wrap: break-word;
+    }
+  `
 })
 export class NgxOmniviewComponent {
+  /**
+   * The raw string content to be rendered
+   */
+  @Input() data: string = '';
 
+  /**
+   * The format/type of the content
+   * @default 'text'
+   */
+  @Input() format: OmniviewFormat = 'text';
+
+  /**
+   * Get the rendered content based on the format
+   */
+  get renderedContent(): string {
+    if (!this.data) {
+      return '';
+    }
+
+    switch (this.format) {
+      case 'text':
+        return this.renderText();
+      
+      // TODO: Add more renderers
+      case 'html':
+      case 'markdown':
+      case 'latex':
+      case 'mathjax':
+      case 'json':
+      case 'code':
+        return `[${this.format} rendering - coming soon]\n\n${this.data}`;
+      
+      default:
+        return this.data;
+    }
+  }
+
+  /**
+   * Render plain text (simplest renderer)
+   */
+  private renderText(): string {
+    return this.data;
+  }
 }
